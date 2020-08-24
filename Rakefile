575ec2b929bae212667c1cdb7fc121429596145c
plaisirgs <63075823+plaisirgs@users.noreply.github.com>
2020-08-24 17:03:03 -0400

    WIP

--- Rakefile
+++ Rakefile
@@ -2,9 +2,15 @@ ENV["SINATRA_ENV"] ||= "development"
 
 require_relative './config/environment'
 require 'sinatra/activerecord/rake'
+#require_relative "./db/seeds.rb"
+
 
 
 task :console do
   ActiveRecord::Base.logger = Logger.new(STDOUT)
   Pry.start
+end
+
+task :seed do
+  ruby "db/seeds.rb"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 
-c2 = Course.create(title: "Introduction to Information Technology", professor: "Luis Brown")
-c3 = Course.create(title: "Introduction to Analytics", professor: "Mary Randal")
-c4 = Course.create(title: "Cybersecurity", professor: "Sierra Swift")
-c5 = Course.create(title: "Intro to SQL", professor: "Dente Cruz")
-c6 = Course.create(title: "Project Management", professor: "Olu Bhal")
-c7 = Course.create(title: "Technology Abroad", professor: "Puran Saed")
+
+c2 = Course.create(title: "Introduction to Information Technology", professor: "Luis Brown", rating: 5, category_id: 1000) 
+c3 = Course.create(title: "Introduction to Analytics", professor: "Mary Randal", rating: 4, category_id: 1002)
+c4 = Course.create(title: "Cybersecurity", professor: "Sierra Swift", rating: 4, category_id: 2001)
+c5 = Course.create(title: "Intro to SQL", professor: "Dente Cruz", rating: 3, category_id: 1003)
+c6 = Course.create(title: "Project Management", professor: "Olu Bhal", rating: 2, category_id: 3001)
+c7 = Course.create(title: "Technology Abroad", professor: "Puran Saed", rating: 5, category_id: 2002)
 
